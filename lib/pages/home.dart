2f7c54099628b4ccd5e2c0cef1114b8c486291ef
Blessed Sibanda <blessedsibanda.me@gmail.com>
2021-11-30 13:08:20 +0200

    buttons

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -50,6 +50,7 @@ class _HomeState extends State<Home> {
           child: SingleChildScrollView(
             child: Column(
               children: const [
+                ButtonsRowsWidget(),
                 ContainerWithBoxDecorationWidget(),
                 ColumnWidget(),
                 Divider(),
@@ -218,6 +219,64 @@ class ContainerWithBoxDecorationWidget extends StatelessWidget {
               ]),
         ),
       ),
+    );
+  }
+}
+
+class ButtonsRowsWidget extends StatelessWidget {
+  const ButtonsRowsWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            TextButton(onPressed: () {}, child: const Text('Flag')),
+            TextButton(
+              onPressed: () {},
+              child: const Icon(Icons.flag, color: Colors.white),
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.lightGreen,
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(height: 16.0),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            MaterialButton(
+              onPressed: () {},
+              child: const Text('Save'),
+              color: Colors.grey.shade300,
+            ),
+            MaterialButton(
+              onPressed: () {},
+              child: const Icon(Icons.save),
+              color: Colors.lightGreen,
+            ),
+          ],
+        ),
+        const SizedBox(height: 16.0),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.flight),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.flight, color: Colors.lightGreen),
+              color: Colors.white,
+              tooltip: 'Flight',
+              iconSize: 42.0,
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
