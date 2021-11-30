6fdd6cc49dcfbff967dea3b0093cdc6c6e53f950
Blessed Sibanda <blessedsibanda.me@gmail.com>
2021-11-30 11:04:39 +0200

    customize app bar

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.indigo,
+        primarySwatch: Colors.lightGreen,
       ),
       home: const Home(),
     );
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -13,6 +13,36 @@ class _HomeState extends State<Home> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home'),
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.menu),
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: const Icon(Icons.more_vert),
+            onPressed: () {},
+          ),
+        ],
+        flexibleSpace: const SafeArea(
+          child: Icon(
+            Icons.photo_camera,
+            size: 75.0,
+            color: Colors.white70,
+          ),
+        ),
+        bottom: PreferredSize(
+          child: Container(
+            color: Colors.lightGreen.shade100,
+            height: 75.0,
+            width: double.infinity,
+            child: const Text('Bottom'),
+          ),
+          preferredSize: const Size.fromHeight(75.0),
+        ),
       ),
       body: Container(),
     );
