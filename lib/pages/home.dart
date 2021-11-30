92422c87c52f2850b9bec05e619ada874b7b3cb7
Blessed Sibanda <blessedsibanda.me@gmail.com>
2021-11-30 11:23:19 +0200

    create decorated container widget, & center bottom text

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -39,12 +39,63 @@ class _HomeState extends State<Home> {
             color: Colors.lightGreen.shade100,
             height: 75.0,
             width: double.infinity,
-            child: const Text('Bottom'),
+            child: const Center(child: Text('Bottom')),
           ),
           preferredSize: const Size.fromHeight(75.0),
         ),
       ),
-      body: Container(),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SafeArea(
+          child: SingleChildScrollView(
+            child: Column(
+              children: const [
+                ContainerWithBoxDecorationWidget(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ContainerWithBoxDecorationWidget extends StatelessWidget {
+  const ContainerWithBoxDecorationWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      decoration: BoxDecoration(
+        borderRadius: const BorderRadius.only(
+          bottomLeft: Radius.circular(100.0),
+          bottomRight: Radius.circular(10.0),
+        ),
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Colors.white,
+            Colors.lightGreen.shade500,
+          ],
+        ),
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.white,
+            blurRadius: 10.0,
+            offset: Offset(0.0, 10.0),
+          ),
+        ],
+      ),
+      child: Center(
+        child: RichText(
+          text: const TextSpan(
+            text: 'Container',
+            style: TextStyle(color: Colors.black),
+          ),
+        ),
+      ),
     );
   }
 }
